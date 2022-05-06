3e609765972d7bf501a77d4df370471ed629c9c3
rosemutai <chepngetichrose2030@gmail.com>
2022-05-06 18:44:32 +0300

    customized tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+
+  darkMode: 'class',
+
+  theme: {
+    extend: {
+      colors: {
+        'blackish': '#0f0e17',
+        'whitish': '#a7a9be',
+        'orangish': '#ff8906'
+      },
+    },
+  },
+  plugins: [],
+}
+
