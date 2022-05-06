180a495cc6dd06e7cfbf0213ab86288049fa3268
rosemutai <chepngetichrose2030@gmail.com>
2022-05-06 18:41:53 +0300

    light-dark mode

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Background from './components/Background';
+import { ThemeProvider } from './components/ThemeContext';
+import Toggle from './components/ThemeToggle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <Background>
+        <div className="absolute right-0 top-0 mr-4 mt-4 md:mr-6 md:mt-6">
+          <Toggle />
+        </div>
+        <App />
+      </Background>
+    </ThemeProvider>
+    
   </React.StrictMode>
 );
 
